39841e58cf7e6d2f28babbac6293980a56d4ad30
Asis <asisloustau@gmail.com>
2019-12-19 13:03:10 -0500

    Started scrape_flights function

--- webscraper_cheapflights/scraper.py
+++ webscraper_cheapflights/scraper.py
@@ -13,6 +13,7 @@ import pandas as pd
 import numpy as np 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 # missing: email management,
 
 ### to-do:create pseudocode for functions
@@ -21,14 +22,13 @@ def search_flights():
     '''
     This function will take care of the input values for the website and click search
     '''
-    from_city = "San Diego"
+    from_city = "LAX"
     to_city = "Madrid"
-    date_from = "Jun 10" # we can also use datetime format and convert to this type, this is what Google Flights uses
-    date_to = "Jun 30"
+    date_from = "Jun 9" # we can also use datetime format and convert to this type, this is what Google Flights uses
+    date_to = "Jun 21"
 
-    # add: connect using proxy
-    driver = webdriver.Firefox()
-    driver.get("https://www.google.com/flights")
+    # add: connect using proxy 
+ 
     sleep(2)
     ### example of first input: from field
     from_xpath = "/html/body/div[2]/div[2]/div/div[2]/div[3]/div/jsl/div/div[2]/main[1]/div[4]/div/div[3]/div/div[2]/div[1]"
@@ -67,7 +67,7 @@ def search_flights():
 
     ### date_to
 
-    sleep(1)
+    sleep(randint(1,4))
     date_to_xpath_2 = "/html/body/div[2]/div[2]/div/div[2]/div[3]/div/jsl/div/div[5]/div/div[4]/div[2]/div[3]/date-input/input"
     date_to_input_2 = driver.find_element_by_xpath(date_to_xpath_2)
     date_to_input_2.clear()
@@ -80,14 +80,29 @@ def search_flights():
     sleep(1)
     element = driver.switch_to.active_element.send_keys(Keys.RETURN) # press return in button Done -switched to this by pressing TAB
 
+    ### sort flights by price
+    sleep(2) # use wait function instead
+    sort_button = driver.find_element_by_css_selector(".gws-flights-results__sort-menu-icon")
+    sort_button.click()
+    sleep(1)
+    sort_by_price_xpath = "/html/body/div[2]/div[2]/div/div[2]/div[3]/div/jsl/div/div[2]/main[4]/div[7]/div[1]/div[5]/div[1]/div/div/dropdown-menu/div/div[2]/menu-item[2]"
+    driver.find_element_by_xpath(sort_by_price_xpath).click()
     sleep(10) # sleep a couple of seconds between each input
-    driver.close()
+    
 
 def scrape_flights():
     '''
     This functin will scrape the data from the website and create a DataFrame with the content
     '''
-    pass
+    # TO-DO: sort by cheapest -dropdown: use XPATH /html/body/div[2]/div[2]/div/div[2]/div[3]/div/jsl/div/div[2]/main[4]/div[7]/div[1]/div[5]/div[1]/div/div/dropdown-menu/div/div[1]/span[1]
+    print("Inside scrape_flights")
+    soup = BeautifulSoup(driver.page_source,"html.parser")
+    flights = soup.findall("li")
+    print("Number of flights scraped: " + len(flights))
+    for i in flights:
+        print(i)
+    
+
 
 
 # to-do: Proxy implementation
@@ -106,6 +121,17 @@ def scrape_proxy(website = "https://free-proxy-list.net/"):
     driver.close()
     return proxy_list
 
+# proxies = scrape_proxy() eventually we will save this proxies before calling search_flights and use them
+### Open browser instance and Google Flights - driver is global variable so we can access from different functions -> create class instead?
+driver = webdriver.Firefox()
+driver.get("https://www.google.com/flights")
+
+### Web interaction
 search_flights()
-print(scrape_proxy()) #test function
-# function 2: try proxies (Starting with newest) to connect -> this will go in scrape function
+
+### Web scraping
+scrape_flights()
+
+### Close browser
+driver.close()
+
